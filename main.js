229151c33cbba8d7e7eba5e1c2e7e81fe74793b1
kayleekim409 <81453056+kayleekim409@users.noreply.github.com>
2022-07-21 15:54:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function gallery() {
+    window.location = "gallery.html";
+}
+function videos() {
+    window.location = "videos.html";
+}
+function players() {
+    window.location = "players.html";
+}
+function btnBack() {
+    window.location = "index.html";
+}
